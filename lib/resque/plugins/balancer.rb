d9e6ad385fa3fe15bef8ba5f39e97c1d66e93d94
Michael Grosser <michael@grosser.it>
2016-09-05 22:13:30 -0700

    first draft

--- lib/resque/plugins/balancer.rb
+++ lib/resque/plugins/balancer.rb
@@ -3,7 +3,17 @@ require 'resque'
 module Resque
   module Plugins
     module Balancer
+      def queues
+        super.sort_by { |q| - weights[q].to_f } # TODO: nil = 1
+      end
 
+      # TODO: dynamic
+      def weights
+        @weights ||= begin
+          weights = ENV['WEIGHTS'] || raise(KeyError, 'resque-balancer needs WEIGHTS to work')
+          weights.split(',').map { |w| w.split(':') }.to_h
+        end
+      end
     end
   end
 end
--- spec/resque/balancer_spec.rb
+++ spec/resque/balancer_spec.rb
@@ -13,13 +13,11 @@ end
 Resque.logger = InstantFeedbackLogger
 
 describe Resque::Plugins::Balancer do
-  def worker(queues)
-    worker = Resque::Worker.new(*queues)
-    worker.fork_per_job = false # make our assertions work
-    worker
+  class AllJob
+    def self.perform(queue)
+      raise "Unexpected perform #{queue}"
+    end
   end
-
-  let(:single_run) { 0 }
 
   class TestJob
     def self.queue
@@ -27,7 +25,7 @@ describe Resque::Plugins::Balancer do
     end
 
     def self.perform
-      raise "Unexpected perform"
+      AllJob.perform(queue)
     end
   end
 
@@ -37,27 +35,53 @@ describe Resque::Plugins::Balancer do
     end
 
     def self.perform
-      raise "Unexpected perform"
+      AllJob.perform(queue)
     end
   end
+
+  def worker(queues)
+    worker = Resque::Worker.new(*queues)
+    worker.fork_per_job = false # make our assertions work
+    worker
+  end
+
+  def with_env(env)
+    old = env.keys.map { |k| [k, ENV[k.to_s]] }
+    env.each { |k, v| ENV[k.to_s] = v }
+    yield
+  ensure
+    old.each { |k, v| ENV[k.to_s] = v }
+  end
+
+  let(:single_run) { 0 }
 
   it "has a VERSION" do
     expect(Resque::Balancer::VERSION).to match /^[\.\da-z]+$/
   end
 
   it "does not blow up" do
-    worker = worker(['test_job'])
-    Resque.enqueue(TestJob)
-    expect(TestJob).to receive(:perform)
-    worker.work(single_run)
+    with_env 'WEIGHTS' => 'other_job:2' do
+      worker = worker(['test_job'])
+      Resque.enqueue(TestJob)
+      expect(AllJob).to receive(:perform).with('test_job')
+      worker.work(single_run)
+    end
+  end
+
+  it "fails with missing WEIGHTS" do
+    expect do
+      worker(['test_job']).work(single_run)
+    end.to raise_error(/WEIGHTS/)
   end
 
   it "enqueues from the highest priority queue when everything is clean" do
-    worker = worker(['other_job', 'test_job'])
-    Resque.enqueue(OtherJob)
-    Resque.enqueue(TestJob)
-    expect(TestJob).to receive(:perform)
-    expect(OtherJob).to_not receive(:perform)
-    worker.work(single_run)
+    with_env 'WEIGHTS' => 'other_job:1,test_job:2' do
+      worker = worker(['other_job', 'test_job'])
+      Resque.enqueue(OtherJob)
+      Resque.enqueue(TestJob)
+      expect(AllJob).to receive(:perform).with('test_job').ordered
+      expect(AllJob).to receive(:perform).with('other_job').ordered
+      worker.work(single_run)
+    end
   end
 end
